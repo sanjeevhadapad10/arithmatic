e27954737bf9f8275f001087a2c4039cf25da4ec
sanjeevhadapda <sanjeevhadapda10@gamil.com>
2025-10-24 10:04:32 +0530

    added multiplication and division equation

--- arithmatics.py
+++ arithmatics.py
@@ -1,4 +1,6 @@
-a = float(input("enter the first number")
-b = float(input("entert the second number")
+a = float(input("enter the first number"))
+b = float(input("entert the second number"))
 print("addition",a+b)
 print("substraction",a-b)
+print("division",a*b)
+
